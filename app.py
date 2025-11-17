833bbf85130f37d8877a67e20464d0b3e1c7610d
Zoe P. <112001279+zoepcbs@users.noreply.github.com>
2025-11-16 21:45:24 -0600

    initialize app

---
+++ app.py
@@ -0,0 +1,24 @@
+import requests
+import pygal
+from lxml import etree
+
+class StockDataVisualizer:
+
+    def __init__(self, api_key):
+
+        self.api_key = api_key
+        self.base_url = "https://www.alphavantage.co/query"
+
+    def get_stock_symbol(self):
+
+        symbol = input("Enter the stock symbol you are looking for: ").upper().strip()
+        return symbol
+    
+    def get_chart_type(self):
+
+        print("\nChart Types:")
+        print("\n--------------")
+        print("1. Bar\n 2. Line")
+
+        chart_type = input("Enter the chart type you want (1,2):")
+        
