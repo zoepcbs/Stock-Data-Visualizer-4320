5ab23daae080b8ae8f2839ca04eb6d0c721b111c
Zoe P. <112001279+zoepcbs@users.noreply.github.com>
2025-11-17 18:16:38 -0600

    getting more inputs

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ class StockDataVisualizer:
 
     def get_stock_symbol(self):
 
-        symbol = input("Enter the stock symbol you are looking for: ").upper().strip()
+        symbol = input("Enter the stock symbol you are looking for:").upper().strip()
         return symbol
     
     def get_chart_type(self):
@@ -20,5 +20,29 @@ class StockDataVisualizer:
         print("\n--------------")
         print("1. Bar\n 2. Line")
 
-        chart_type = input("Enter the chart type you want (1,2):")
+        chart_type = input("Enter the chart type you want (1, 2):").strip()
+
+        if chart_type == "1":
+            return "bar"
+        elif chart_type == "2":
+            return "line"
+        else:
+            return "line"
         
+    def get_time_series(self):
+        print("Select the Time Series of the chart you want to generate:")
+        print("\n----------------------------")
+        print("\n1. Intraday\n2. Daily\n3. Weekly\n4. Monthly\n")
+        
+        time_series = input("Enter time series option (1-4): ").strip()
+
+        if time_series == "1":
+            return "TIME_SERIES_INTRADAY"
+        elif time_series == "2":
+            return "TIME_SERIES_DAILY"
+        elif time_series == "3":
+            return "TIME_SERIES_WEEKLY"
+        elif time_series == "4":
+            return "TIME_SERIES_MONTHLY"
+        else:
+            return "TIME_SERIES_DAILY"
